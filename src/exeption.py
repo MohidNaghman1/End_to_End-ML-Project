0a2254fbd585e3b8ed32e7151c316c137d84ade1
MohidNaghman1 <mohidnaghman315@gmail.com>
2025-09-08 09:29:30 -0700

    removing texting code

--- src/exeption.py
+++ src/exeption.py
@@ -21,9 +21,3 @@ class CustomException(Exception):
     
 
 
-if __name__ == "__main__":
-    try:
-        a = 1/0
-    except Exception as e:
-        logging.info("Divide by zero exception occured")
-        raise CustomException(e,sys)
